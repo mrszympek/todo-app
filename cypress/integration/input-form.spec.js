7d126763eeb6bb48e031d5a4ca1d3abbf4e90fbe
mrszympek <piela.bartek@gmail.com>
2019-03-01 19:41:58 +0100

    tests improvement

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,6 +1,9 @@
 describe('Input form', () => {
-    it('should focus input on load page', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:3030');
+    });
+
+    it('should focus input on load page', () => {
         cy.focused()
             .should('have.class', 'new-todo')
     });
@@ -8,7 +11,6 @@ describe('Input form', () => {
     it('Accept input', () => {
         const inputText = 'Buy some chips';
 
-        cy.visit('http://localhost:3030');
         cy.get('.new-todo')
             .type(inputText)
             .should('have.value', inputText)
