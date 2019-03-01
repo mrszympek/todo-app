1511ec582c8bf42e6144fcceea91461c35b6cbdc
mrszympek <piela.bartek@gmail.com>
2019-03-01 22:13:02 +0100

    tests explanation

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,40 +1,47 @@
 describe('Input form', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3030');
-    });
+  // Before each wykonuje sie dla każdego it w danym kontekscie
+  beforeEach(() => {
+    cy.visit('http://localhost:3030');
+  });
 
-    it('should focus input on load page', () => {
-        cy.focused()
-            .should('have.class', 'new-todo')
-    });
+  // Sprawdzam czy input formularza istnieje na stronie
+  it('should focus input on load page', () => {
+    cy.focused()
+      .should('have.class', 'new-todo')
+  });
 
-    it('Accept input', () => {
-        const inputText = 'Buy some chips';
+  // Sprawdam czy input przyjmuje value które wprowadzamy
+  it('Accept input', () => {
+    const inputText = 'Buy some chips';
 
-        cy.get('.new-todo')
-            .type(inputText)
-            .should('have.value', inputText)
-    });
+    cy.get('.new-todo')
+      .type(inputText)
+      .should('have.value', inputText)
+  });
 
+  // Tworzenie nowego kontekstu dla sprawdzania formualrzy
   context('Form submission', () => {
     it('Add new todo item to the list', () => {
       const todoText = 'Create project on Studies';
       cy.server();
+      // Tworzymy nowy obiekt z itemem i posylamy go postem na server pod adres /api/todos
       cy.route('POST', '/api/todos', {
         name: todoText,
         id: 1,
         isComplete: false
       });
-
+      // Bierzemy selektor newtodo przekauzjemy my wartosc z zmiennej todoText i wysylamy w formlarzu, oczekujemy ze jeko value === todoText
       cy.get('.new-todo')
         .type(todoText)
         .type('{enter}')
         .should('have.value', '');
+      // Bierzemy liste bierzemy liste i sprawdzamy czy element sie dodal, powinien zawiereac wartoc pola todoText
       cy.get('.todo-list li')
         .should('have.length', 1)
         .and('contain', todoText)
     });
 
+    // Test sprawdzaajacy przypadek dostania z serwera 500, jezeli form wysle nowa wartosc ktora sie nie doda sprawdzamy czy error sie wyswietla
     it('Show error when server response is 500', () => {
       cy.server();
       cy.route({
